d05b2fdb9354c09a6f78bb9b27be9cca3de71192
Gregor <gregor.mueller-riederer@itk-engineering.de>
2020-12-31 16:15:23 +0100

    -- fgauge: Added dummy for chargecycle detection

---
+++ src/user/fgauge/fgauge_chargecycle.c
@@ -0,0 +1,11 @@
+#include "fgauge_chargecycle.h"
+
+void fgauge_chargecycle_init(fgauge_cfg_type* cfg)
+{
+    /* TODO */
+}
+
+fgauge_chargestate_type fgauge_chargecycle_main(void)
+{
+    /* TODO */
+}
